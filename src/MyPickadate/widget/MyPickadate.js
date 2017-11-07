5bba0bbcd681f7a2015ec9195cae1995bcbdbb98
Conner Charlebois <cdcharlebois@gmail.com>
2017-11-07 14:01:41 -0500

    add initial files

---
+++ src/MyPickadate/widget/MyPickadate.js
@@ -0,0 +1,78 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+    "MyPickadate/lib/jquery-1.11.2",
+    "dojo/text!MyPickadate/widget/template/MyPickadate.html"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, widgetTemplate) {
+    "use strict";
+
+    var $ = _jQuery.noConflict(true);
+
+    return declare("MyPickadate.widget.MyPickadate", [ _WidgetBase, _TemplatedMixin ], {
+
+        templateString: widgetTemplate,
+
+
+        widgetBase: null,
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function () {
+            this._handles = [];
+        },
+
+        postCreate: function () {
+            logger.debug(this.id + ".postCreate");
+        },
+
+        update: function (obj, callback) {
+            logger.debug(this.id + ".update");
+
+            this._contextObj = obj;
+            this._updateRendering(callback);
+        },
+
+        resize: function (box) {
+          logger.debug(this.id + ".resize");
+        },
+
+        uninitialize: function () {
+          logger.debug(this.id + ".uninitialize");
+        },
+
+        _updateRendering: function (callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            if (this._contextObj !== null) {
+                dojoStyle.set(this.domNode, "display", "block");
+            } else {
+                dojoStyle.set(this.domNode, "display", "none");
+            }
+
+            this._executeCallback(callback);
+        },
+
+        _executeCallback: function (cb) {
+          if (cb && typeof cb === "function") {
+            cb();
+          }
+        }
+    });
+});
+
+require(["MyPickadate/widget/MyPickadate"]);
